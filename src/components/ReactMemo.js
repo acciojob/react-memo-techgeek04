f64f9934dcd75ab2222114baf91dea9954316550
techgeek04 <155185231+techgeek04@users.noreply.github.com>
2025-02-02 22:17:13 +0530

    Update ReactMemo.js

--- src/components/ReactMemo.js
+++ src/components/ReactMemo.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-const ReactMemo = React.memo(({ todos }) => {
-  console.log("Rendering Todo List...");
+const ReactMemo = React.memo(() => {
+  const [skills, setSkills] = useState(["HTML", "CSS", "JavaScript", "React"]);
+  const [newSkill, setNewSkill] = useState("");
+
+  const addSkill = () => {
+    if (newSkill.length > 5) {
+      setSkills([...skills, newSkill]);
+      setNewSkill("");  
+    }
+  };
+
   return (
     <div>
-      <h2>Todo List</h2>
+      <h2>React.memo</h2>
+      <input
+        id="skill-input"
+        type="text"
+        value={newSkill}
+        onChange={(e) => setNewSkill(e.target.value)}
+      />
+      <button id="add-skill" onClick={addSkill}>Add Skill</button>  {/* Ensuring button exists */}
       <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>{todo}</li>
+        {skills.map((skill, index) => (
+          <li key={index}>{skill}</li>
         ))}
       </ul>
     </div>
