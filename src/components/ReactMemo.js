48807c1474d7d8dcbcc4e323e402a6f55ccb06e7
techgeek04 <155185231+techgeek04@users.noreply.github.com>
2025-02-02 22:12:30 +0530

    Update ReactMemo.js

--- src/components/ReactMemo.js
+++ src/components/ReactMemo.js
@@ -1,31 +1,17 @@
-import React, { useState } from "react";
-import TodoList from "./TodoList";
+import React from "react";
 
-const ReactMemo = () => {
-  const [skills, setSkills] = useState(["HTML", "CSS", "JavaScript", "React"]);
-  const [input, setInput] = useState("");
-
-  const addSkill = () => {
-    if (input.length > 5) {
-      setSkills([...skills, input]);
-      setInput("");
-    } else {
-      alert("Skill name must be longer than 5 characters.");
-    }
-  };
-
+const ReactMemo = React.memo(({ todos }) => {
+  console.log("Rendering Todo List...");
   return (
     <div>
-      <h2>React.memo</h2>
-      <input
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <button onClick={addSkill}>Add Skill</button>
-      <TodoList skills={skills} />
+      <h2>Todo List</h2>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
-};
+});
 
 export default ReactMemo;
