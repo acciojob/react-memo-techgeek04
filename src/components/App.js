6d2bf91b7caeb6b3e2e1d347ba05d9650e80cb10
techgeek04 <155185231+techgeek04@users.noreply.github.com>
2025-02-25 23:14:51 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import UseMemo from "./UseMemo";
 import ReactMemo from "./ReactMemo";
 
 const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const onAdd = () => {
+    setTasks((prev) => [...prev, "New Todo"]);
+  };
   return (
-    <div>
-      <h1>React Performance Optimization</h1>
+    <div id="main">
+      <h1>React.useMemo</h1>
+      <h2>My todos</h2>
+      <ul>
+        {tasks.map((i, index) => (
+          <li key={index}>{i}</li>
+        ))}
+      </ul>
+
+      <button onClick={onAdd}>Add Todo</button>
+
+      <hr />
       <UseMemo />
+      <hr />
+      <hr />
       <ReactMemo />
     </div>
   );
