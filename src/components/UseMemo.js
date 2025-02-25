ecd015a35850fc51dea96c6a2709ac1039fbf4d0
techgeek04 <155185231+techgeek04@users.noreply.github.com>
2025-02-25 23:14:08 +0530

    Update UseMemo.js

--- src/components/UseMemo.js
+++ src/components/UseMemo.js
@@ -1,35 +1,23 @@
-import React, { useState, useMemo } from "react";
+import React, { useMemo, useState } from "react";
 
 const UseMemo = () => {
-  const [todos, setTodos] = useState([]);
   const [count, setCount] = useState(0);
 
-  const addTodo = () => {
-    setTodos([...todos, "New todo"]);
+  const result = useMemo(() => {
+    return count * 2;
+  }, [count]);
+
+  const onIncrease = () => {
+    setCount(count + 1);
   };
-
-  const expensiveCalculation = (num) => {
-    console.log("Calculating...");
-    return num;
-  };
-
-  const calculation = useMemo(() => expensiveCalculation(100000000), []);
-
   return (
     <div>
-      <h1>React.useMemo</h1>
-      <h2>My todos</h2>
-      <ul id="todo-list">  
-        {todos.map((todo, index) => (
-          <li key={index}>{todo}</li>
-        ))}
-      </ul>
-      <button id="add-todo" onClick={addTodo}>Add Todo</button> 
-      <br />
-      <p>Count: {count}</p>
-      <button id="increment-count" onClick={() => setCount(count + 1)}>+</button>
+      <div>
+        <span>Count:{count}</span>
+        <button onClick={onIncrease}>+</button>
+      </div>
       <h2>Expensive Calculation</h2>
-      <p>{calculation}</p>
+      {result}
     </div>
   );
 };
